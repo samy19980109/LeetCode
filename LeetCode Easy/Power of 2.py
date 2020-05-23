cb26b37f9d4f6b297badf8937ef2eda105a8505e
Samarth Agarwal <samy19980109@gmail.com>
2020-05-22 23:42:20 -0400

    added explanation for Power of 2

--- LeetCode Easy/Power of 2.py
+++ LeetCode Easy/Power of 2.py
@@ -25,6 +25,9 @@ class Solution:
 
     def isPowerOfTwo2(self, n):
         """
+        n & n - 1 removes the left most bit of n. If an integer is power of 2, there is a single bit in the
+        binary representation of n. e.g. 16 = b10000, 16 - 1 = b01111, and 16 & 16 - 1 = b10000 & b01111 = 0,
+        also 16 != 0, based on these facts there is only one bit in b10000, so 16 is power of 2.
         :type n: int
         :rtype: bool
         """
